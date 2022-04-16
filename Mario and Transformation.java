e17dfc68cccace6eff1a96ae5221fff0d5a5d1b0
Raj sinha <94060322+r-a-j-s-i-n-h-a@users.noreply.github.com>
2022-04-16 22:40:10 +0530

    Mario transforms each time he eats a mushroom as follows:  If he is currently small, he turns normal. If he is currently normal, he turns huge. If he is currently huge, he turns small. Given that Mario was initially normal, find his size after eating X mushrooms.

---
+++ Mario and Transformation.java
@@ -0,0 +1,33 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		try{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+	while(t!=0){
+	    int x=sc.nextInt();
+	    if(x%3==2){
+	        System.out.println("SMALL");
+	    }
+	    if(x%3==1){
+	        System.out.println("HUGE");
+	    }
+	    if(x%3==0){
+	        System.out.println("NORMAL");
+	    }
+	}
+		}
+		catch(Exception e){
+		    
+		}
+	}
+}

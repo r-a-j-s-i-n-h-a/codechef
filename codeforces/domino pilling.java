64601a661118cc3f80c53de4d50079c88f152834
Raj sinha <94060322+r-a-j-s-i-n-h-a@users.noreply.github.com>
2022-09-10 19:10:10 +0530

    Create domino pilling.java

---
+++ codeforces/domino pilling.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println((n*b)/2);
+
+
+
+    }
+    }
+

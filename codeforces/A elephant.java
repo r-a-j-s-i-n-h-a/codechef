e2953e0d2ad0b5fc48e34f01c024491c87399f93
Raj sinha <94060322+r-a-j-s-i-n-h-a@users.noreply.github.com>
2022-09-15 23:31:52 +0530

    Create  A elephant.java

---
+++ codeforces/A elephant.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class main{
+    public static void main(String args[]){
+        Scanner sc =new Scanner(System.in);
+        int a=sc.nextInt();
+        int count=0;
+        if(a%5==0){
+            System.out.println(1);
+            count=a/5;
+        }
+        else{
+            count = (a/5)+1;
+        }
+
+        System.out.println(count);
+    }
+}
